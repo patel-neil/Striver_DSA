a184c4f663bf0ccccda5ce2e7982a63b86615678
patel-neil <neilpatel400271@gmail.com>
2026-05-22 11:08:05 +0530

    Completed all pattern problems.

--- Basics/patterns.cpp
+++ Basics/patterns.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
 int main()
@@ -251,4 +251,252 @@ int main()
     //     flag = true;
     //     cout << endl;
     // }
+
+
+    /*
+    1             1
+    1 2         2 1
+    1 2 3     3 2 1
+    1 2 3 4 4 3 2 1
+    */
+
+    // for(int i=0; i<n; i++)
+    // {
+    //     for(int j=0; j<i+1; j++)
+    //     {
+    //         cout << j+1 << " ";   
+    //     }
+    //     for(int j=0; j<2*(n-i-1); j++)
+    //     {
+    //         cout << "  ";
+    //     }
+    //     for(int j=i+1; j>0; j--)
+    //     {
+    //         cout << j << " ";   
+    //     }
+    //     cout << endl;
+    // }
+
+    /*
+    1
+    2 3
+    4 5 6
+    7 8 9 10
+    11 12 13 14 15
+    */
+   
+    // int count = 1;
+    // for(int i=0; i<n; i++)
+    // {
+    //     for(int j=0; j<n;j++)
+    //     {
+    //         if(i >= j)
+    //         {
+    //             cout << count << " ";
+    //             count++;
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+
+    /*
+    E
+    D E
+    C D E
+    B C D E
+    A B C D E
+    */
+
+    // char alpha = 'A';
+
+    // for(int i=0; i<n; i++)
+    // {
+    //     for(int j=0; j<n; j++)
+    //     {
+    //         if(i >= j)
+    //         {
+    //             cout << (char)(alpha + n - i + j - 1) << " "; 
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+
+    /*
+    * * * * * *
+    * *     * *
+    *         *
+    *         *
+    * *     * *
+    * * * * * *
+    */
+
+    // for(int i=0; i<n; i++)
+    // {
+    //     for(int j=0; j<(n-i); j++)
+    //     {
+    //         cout << "*" << " ";
+    //     }
+    //     for(int j=0; j<2*i; j++)
+    //     {
+    //         cout << "  ";
+    //     }
+    //     for(int j=0; j<(n-i); j++)
+    //     {
+    //         cout << "*" << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // for(int i=n-1; i>=0; i--)
+    // {
+    //     for(int j=0; j<(n-i); j++)
+    //     {
+    //         cout << "*" << " ";
+    //     }
+    //     for(int j=0; j<2*i; j++)
+    //     {
+    //         cout << "  ";
+    //     }
+    //     for(int j=0; j<(n-i); j++)
+    //     {
+    //         cout << "*" << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    /*
+    *         *
+    * *     * *
+    * * * * * *
+    * *     * *
+    *         *
+    */
+
+    // for(int i=n-1; i>=0; i--)
+    // {
+    //     for(int j=0; j<(n-i); j++)
+    //     {
+    //         cout << "*" << " ";
+    //     }
+    //     for(int j=0; j<2*i; j++)
+    //     {
+    //         cout << "  ";
+    //     }
+    //     for(int j=0; j<(n-i); j++)
+    //     {
+    //         cout << "*" << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // for(int i=1; i<n; i++)
+    // {
+    //     for(int j=0; j<(n-i); j++)
+    //     {
+    //         cout << "*" << " ";
+    //     }
+    //     for(int j=0; j<2*i; j++)
+    //     {
+    //         cout << "  ";
+    //     }
+    //     for(int j=0; j<(n-i); j++)
+    //     {
+    //         cout << "*" << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    /*
+    * * * *
+    *     *
+    *     *
+    * * * *
+    */
+
+    // for(int i=0; i<n; i++)
+    // {
+    //     if(i ==0 || i== (n-1))
+    //     {
+    //         for(int j=0; j<n; j++)
+    //         {
+    //             cout << "*" << " ";
+    //         }
+    //     }
+    //     else
+    //     {
+    //         for(int j=0; j<n; j++)
+    //         {
+    //             if(j == 0 || j == (n-1))
+    //             {
+    //                 cout << "*" << " ";
+    //             }
+    //             else{
+    //                 cout << "  ";
+    //             }
+    //         }
+    //     }
+
+    //     cout << endl;
+    // }
+
+    /*
+    3 3 3 3 3
+    3 2 2 2 3
+    3 2 1 2 3
+    3 2 2 2 3
+    3 3 3 3 3
+    */
+
+    //Code not working
+    // for(int i=0; i<(2*n)-1; i++)
+    // {
+    //     for(int k=n; k>0; k--)
+    //     {
+    //         if(i == 0 || i == (2*n)-2)
+    //         {
+    //             for(int j=0; j<(2*n)-1; j++)
+    //             {
+    //                 cout << k << " ";
+    //             }
+    //         }
+    //         else
+    //         {
+    //             for(int j=0; j<2*n-1; j++)
+    //             {
+    //                 if(j == 0 || j == 2*(n-1))
+    //                 {
+    //                     cout << k << " ";
+    //                 }
+    //                 else{
+    //                     cout << "  ";
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+
+    //Working code
+
+    //Distance
+
+    int top, bottom, right, left;
+
+    for(int i=0; i< 2*n-1; i++)
+    {
+        for(int j=0; j< 2*n-1; j++)
+        {
+            top = i;
+            bottom = 2*(n-1) - i;
+            left = j;
+            right = 2*(n-1) - j;
+
+            int min_dist = min(min(top,bottom), min(left, right));
+
+            int value = n- min_dist;
+
+            cout << value << " ";
+        }
+        cout << endl;
+    }
 }
