dfb9154322025d2ac3e1ddbef16df9b5d104461a
Neil Patel <131244579+patel-neil@users.noreply.github.com>
2026-07-22 16:10:25 +0530

    Time: 0 ms (100%), Space: 27.2 MB (81.51%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        int left = 0, right = n - 1;
+
+        while(left <= right)
+        {
+            int temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+};
