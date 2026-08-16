72edff7acaf308d6ffa4c118b83363261d46a857
Neil Patel <131244579+patel-neil@users.noreply.github.com>
2026-08-16 19:16:00 +0530

    Time: 0 ms (100%), Space: 7.8 MB (50.44%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
 
-        if(n <= 0) return false;
-        double m = log2(n);
-
-        int new1 = (int)m;
-
-        
-
-        if(m == new1)
+        if(n <= 0)
         {
-            return true;
+            return false;
         }
+
+        if((n & (n - 1)) == 0)
+            return true;
+
         return false;
     }
 };
