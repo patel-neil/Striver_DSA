a3dea634c1b3ba70bc8c1382224ea5bd460a3b4b
Neil Patel <131244579+patel-neil@users.noreply.github.com>
2026-09-07 19:35:55 +0530

    Time: 36 ms (79.19%), Space: 84.9 MB (62.86%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        
+        int l =0, r=0, maxLen = 0;
+        int k = 2;
+        unordered_map<int,int> mp;
+        while(r < fruits.size())
+        {
+            mp[fruits[r]]++;
+
+            if(mp.size() > k)
+            {
+                while(mp.size() > k)
+                {
+                    mp[fruits[l]]--;
+                    if(mp[fruits[l]] == 0)
+                    {
+                        mp.erase(fruits[l]);
+                    }
+                    l++;
+                }
+            }
+            if(mp.size() <= k)
+            {
+                maxLen = max(maxLen, r - l + 1);
+            }
+            r++;
+        }
+
+        return maxLen;
+    }
+};
